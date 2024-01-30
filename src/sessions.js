0677daca664fd8ad8b58d026b485dfceeebc307f
Max Henrique <max.h.siemermann@gmail.com>
2024-01-29 22:29:46 -0300

    fix: correcao do webhook

--- src/sessions.js
+++ src/sessions.js
@@ -171,7 +171,7 @@ const setupSession = (sessionId, webhookUrl) => {
 			.initialize()
 			.catch((err) => console.log("Initialize error:", err.message));
 
-		if (session?.webhookUrl !== webhookUrl) {
+		if (webhookUrl && session?.webhookUrl !== webhookUrl) {
 			addSession(sessionId, webhookUrl, userAgent?.toString());
 		}
 
